e21ad2fe6cdb200fd1cd26eda9e6c504a4db2ce1
digital8now <101214519+digital8now@users.noreply.github.com>
2023-06-29 18:44:12 -0300

    mobile effects transitions

--- script.js
+++ script.js
@@ -3,4 +3,21 @@ window.onload = function() {
 };
 
 
+function isElementInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
+function animateIfVisible() {
+  var element = document.getElementById('w');
+  if (isElementInViewport(element)) {
+    element.classList.add('visible');
+  }
+}
+
+document.addEventListener('scroll', animateIfVisible);
